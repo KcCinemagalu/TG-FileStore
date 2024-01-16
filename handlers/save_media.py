7a4aafcb32be021adb38b1dbd682bfcec82d480d
KcCinemagalu <124599828+KcCinemagalu@users.noreply.github.com>
2024-01-16 11:07:18 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -74,8 +74,8 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Original Link", url=share_link),
                   InlineKeyboardButton("Short Link", url=short_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/TeleRoidGroup"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/TeleRoid14")]]
+                 [InlineKeyboardButton("Movie Updates", url="https://t.me/Kc_Films_2024"),
+                  InlineKeyboardButton("Admin Support", url="https://t.me/KingKc18")]]
             ),
             disable_web_page_preview=True
         )
@@ -116,8 +116,8 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             reply_markup=InlineKeyboardMarkup(
                [[InlineKeyboardButton("Original Link", url=share_link),
                   InlineKeyboardButton("Short Link", url=short_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/TeleRoidGroup"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/TeleRoid14")]]
+                 [InlineKeyboardButton("Movie Updates", url="https://t.me/Kc_Films_2024"),
+                  InlineKeyboardButton("Admin Support", url="https://t.me/KingKc18")]]
             ),
             disable_web_page_preview=True
         )
